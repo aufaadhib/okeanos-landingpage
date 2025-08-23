37bae61c9b2182938c5b823279a33ed19f93dab8
aufaadhib <faufaadhib@gmail.com>
2025-08-24 01:07:08 +0700

    Penambahan Service Dan Devisi

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import Hero from './components/Hero'
 import { Scrollbars } from "react-custom-scrollbars-4";
 import AboutSection from './components/AboutSection';
 import PortfolioSection from './components/PortfolioSection';
-import { ServicesSection } from './components/ServicesSection';
+import { VideoSection } from './components/VideoSection';
+import ServicesSection from './components/ServicesSection';
+import DevisionSection from './components/DevisionSection';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,9 +20,10 @@ function App() {
       <div className="absolute inset-0 bg-gradient-to-r from-accent/5 to-transparent pointer-events-none" />
         <AboutSection/>
         <PortfolioSection/>
-
       </div>
       <ServicesSection/>
+      <DevisionSection/>
+      <VideoSection/>
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  preview: {
+    allowedHosts: [".ngrok-free.app"], // semua subdomain ngrok-free.app diizinkan
+  },
   plugins: [react(), tailwindcss()],
 });
